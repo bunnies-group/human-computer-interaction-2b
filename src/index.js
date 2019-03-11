6b20d6ecc51b19787ed7aa2fc3abf1ca14ad8659
mciuchitu <xscrub96x@gmail.com>
2019-03-11 11:11:24 +0200

    calculate estimated time

--- src/index.js
+++ src/index.js
@@ -5,15 +5,28 @@ import Util from './util';
 
 const eventController = new EventController();
 
-const [circleStart, circleFinish] = Util.getElementsById(['circleStart', 'circleFinish']);
+const [circleStart, circleFinish, estimatedTime, sizeField, distanceField] = Util.getElementsById([
+  'circleStart', 'circleFinish', 'estimatedTime', 'sizeField', 'distanceField'
+]);
 
-const answer = Util.askValidValue('Enter the diameter of the circles: ', ['5', '20']);
+const answer = Util.askValidValue('Enter the diameter of the circles (5 or 20)', ['5', '20']);
 const circleSize = answer + 'mm';
 
 [circleStart, circleFinish].forEach(it => {
   it.style.height = circleSize;
   it.style.width = circleSize;
 });
+
+const { offsetWidth: size, offsetLeft: offsetStart } = circleStart;
+const { offsetLeft: offsetFinish } = circleFinish;
+const distance = offsetFinish - offsetStart;
+
+sizeField.innerText = size;
+distanceField.innerText = distance;
+
+const a = 50, b = 150;
+
+estimatedTime.innerText = a + b * Math.log2(distance / size + 1);
 
 circleStart.onclick = () => eventController.onStartClick();
 circleFinish.onclick = () => eventController.onFinishClick();
