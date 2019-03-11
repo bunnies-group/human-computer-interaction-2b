72e04debb2539b30588123f60e61d44b5080c264
mciuchitu <xscrub96x@gmail.com>
2019-03-11 11:09:24 +0200

    remove unused methods from Util

--- src/util.js
+++ src/util.js
@@ -2,25 +2,8 @@
 
 export default class {
   /**
-   * @param {Number} from
-   * @param {Number} to
-   * @return {Number}
-   */
-  static randomInt({ from = 0, to = 10000 } = {}) {
-    return Math.floor(from + Math.random() * (to - from));
-  }
-
-  /**
-   * @param {Number} timeout
-   * @return {Promise}
-   */
-  static setTimeoutPromise(timeout) {
-    return new Promise(resolve => setTimeout(resolve, timeout));
-  }
-
-  /**
    * @param {String[]} ids
-   * @param {Document} parent
+   * @param {Document|HTMLElement} parent
    * @return {HTMLElement[]}
    */
   static getElementsById(ids, parent = document) {
